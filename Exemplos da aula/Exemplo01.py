02ae8f95c9498fa2796070c2f86a1cb31165339b
João Victor S2 <112714646+joaoS2@users.noreply.github.com>
2022-09-19 12:49:52 -0300

    Add files via upload

---
+++ Exemplos da aula/Exemplo01.py
@@ -0,0 +1,10 @@
+me= float(input("Insira sua média: "))
+if me == 10:
+    print("Você é um dos meus, goxtozú")
+elif me <= 5:
+    print("Você é um lixo, reprovado!")
+else:
+    print("Você ainda é im lixo, mas dessa vez é recliclável S2")    
+
+    
+
---
+++ Exemplos da aula/Exemplo02.py
@@ -0,0 +1,16 @@
+#Entrada
+N1= float(input("Insira a primeira nota: "))
+N2= float(input("Insira a segunda nota: "))
+#Processamento
+me=(N1+N2)/2
+#Saída
+
+if me == 10:
+    print("esse cara é bom!")
+
+elif me >= 6 :
+    print("você é ruim, mas pode melhorar")
+
+else:
+    print("desiste, minha avó morta é melhor que vc, e olha que na época dela nem tinha isso.")
+      
